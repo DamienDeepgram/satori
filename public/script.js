1da95e97fcb857fcebfeff51bd70bc8d4d623196
Vinny <>
2023-06-13 14:06:53 -0700

    remove unused code

--- public/script.js
+++ public/script.js
@@ -1,15 +1,12 @@
 let socket;
 let mediaRecorder;
 
-let paragraphs = [];
-let spans = [];
 let conversation = document.getElementById('conversation');
 let mic = document.getElementById('mic');
 let offset = 300;
 let scrollOverride = false;
-let currentText = 0;
-const apiOrigin = "http://localhost:3000";
 let recording = false;
+const apiOrigin = "http://localhost:3000";
 
 navigator.mediaDevices
   .getUserMedia({ audio: true })
@@ -52,32 +49,6 @@ function loadWords(p, words, index){
     }
 }
 
-function loadText(){
-    paragraphs = [...document.querySelectorAll('p')];
-    paragraphs.forEach(paragraph => {
-        let htmlString = '';
-        let pArray = paragraph.textContent.split ('');
-        for(let i = 0; i < pArray.length; i++){
-            htmlString += `<span>${pArray[i]}</span>`;
-            paragraph.innerHTML = htmlString;
-        }
-    });
-
-    spans = [...document.querySelectorAll('span')];
-}
-
-function revealSpans(){
-    for(let i = 0; i < spans.length; i++){
-        if ((spans[i].parentElement.getBoundingClientRect().top - offset) < conversation.offsetHeight / 2) {
-            let {left, top} = spans[i].getBoundingClientRect();
-            top = top - (conversation.offsetHeight * 0.2) - offset;
-            let opacityValue = 1 - ( (top * .01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ( (top * .01) + (left * 0.001)).toFixed(3);
-            opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed (3);
-            spans[i].style.opacity = opacityValue;
-        }
-    }
-}
-
 async function promptAI(msg) {
     let model = document.getElementById('model').value;
     const response = await fetch(`${apiOrigin}/chat?model=${model}&message=${encodeURIComponent(msg)}`, {
@@ -89,7 +60,6 @@ async function promptAI(msg) {
     // Make sure to configure your OpenAI API Key in config.json for this to work
     if(data && !data.err){
       let reply = data.response.data.content;
-
       addText(reply, true);
     } else {
       alert('Error: You must configure your OpenAI API Key in the config.json to use the "Respond with AI" feature.');
